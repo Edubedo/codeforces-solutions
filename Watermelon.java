0ca5f2695c53db576e03414743e3cc85b230e926
eescobedo <ezescobedo27@gmail.com>
2025-02-23 19:20:26 -0600

    Watermelon

---
+++ Watermelon.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Get the w
+        int w = scanner.nextInt();
+        // Must be event and greater than 1
+        if((w%2==0) && w > 2) { // We dont accept ood numbers
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
